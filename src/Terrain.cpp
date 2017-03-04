8cdfeab1b77f8468a73cb69d3426bfececc4fac9
Oliver Eisenberg <oeisenbe@usc.edu>
2017-03-04 13:55:09 -0800

    Added ignore file

---
+++ src/Terrain.cpp
@@ -0,0 +1,22 @@
+#include "Terrain.h"
+#include "ofMain.h"
+
+Terrain::Terrain(musicAnalysis*)
+{
+
+}
+
+Terrain::changeColor()
+{
+
+}
+
+Terrain::changeHeihgt()
+{
+
+}
+
+Terrain::draw()
+{
+
+}
--- src/Terrain.h
+++ src/Terrain.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#include "ofMain.h"
+
+class Terrain
+{
+public:
+	Terrain(musicAnalysis*);
+	changeColor();
+	changeHeihgt();
+	draw();
+private:
+	ofMesh mesh;
+	MusicAnalysis* musicAnalysis;
+};
