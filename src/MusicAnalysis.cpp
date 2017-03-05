4f89031d12f616e296aff78a4cbe21f4d67f9bdd
Bryce Matsumori <bmats@users.noreply.github.com>
2017-03-04 17:05:51 -0800

    Add untested range volume

--- src/MusicAnalysis.cpp
+++ src/MusicAnalysis.cpp
@@ -2,36 +2,52 @@
 
 MusicAnalysis::MusicAnalysis(string filename)
 {
-	samplingBands = 512;
-	sound_player.load(filename);
-	sound_player.play();
-}
+	soundPlayer.load(filename);
+	soundPlayer.play();
 
-int getSamplingBands()
-{
-	return samplingBands;
+	// Get the sample rate for the player
+	//shared_ptr<ofFmodSoundPlayer> fmodPlayer = std::static_pointer_cast<OF_SOUND_PLAYER_TYPE>(soundPlayer.getPlayer());
+	//sampleRate = fmodPlayer->internalFreq;
+	sampleRate = 44100;
 }
 
 //returns current volume
 float MusicAnalysis::getVolume()
 {
-	return sound_player.getVolume();
+	return soundPlayer.getVolume();
 }
 
 //returns current pitch
-float* MusicAnalysis::getPitch()
+float MusicAnalysis::getPitch()
 {
-	return sound_player.ofSoundGetSpectrum();
+	// TODO: implement
+	return 0;
 }
 
 //returns current beat
-float MusicAnalysis::getBeat()
+bool MusicAnalysis::getBeat()
 {
-	return sound_player.getBeat();
+	// TODO: implement
+	return false;
 }
 
 //returns volume based on range of pitches given
 float MusicAnalysis::getVolumeOfRange(float min, float max)
 {
-	
+	float* spectrum = ofSoundGetSpectrum(SAMPLING_BANDS);
+
+	// Get the start and end positions in the spectrum
+	int startIndex = static_cast<int>(min / sampleRate * SAMPLING_BANDS);
+	int endIndex = static_cast<int>(max / sampleRate * SAMPLING_BANDS);
+	assert(startIndex < endIndex);
+	assert(startIndex >= 0 && startIndex < SAMPLING_BANDS);
+	assert(endIndex >= 0 && endIndex < SAMPLING_BANDS);
+
+	// Iterate through the spectrum range and add up the volumes
+	float volume = 0;
+	for (int i = startIndex; i <= endIndex; i++)
+	{
+		volume += spectrum[i];
+	}
+	return volume;
 }
--- src/MusicAnalysis.h
+++ src/MusicAnalysis.h
@@ -4,14 +4,15 @@
 
 class MusicAnalysis {
 public:
+	const int SAMPLING_BANDS = 512;
+
 	MusicAnalysis(string filename);
 	float getVolume();
-	float* getPitch();
-	float getBeat();
+	float getPitch();
+	bool getBeat();
 	float getVolumeOfRange(float min, float max);
 	
 private:
 	ofSoundPlayer soundPlayer;
-	int samplingBands;
-
+	float sampleRate;
 };
