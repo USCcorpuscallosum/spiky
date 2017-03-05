5b2e7fa8b426ec7e0f6c8cf907e9aeb85016c7a8
Jenna Permut <permut@usc.edu>
2017-03-04 17:25:11 -0800

    changed background

--- src/Background.cpp
+++ src/Background.cpp
@@ -1,12 +1,20 @@
 #include "Background.h"
 #include "ofMain.h"
 
-Background::Background(MusicAnalysis*);
+Background::Background(MusicAnalysis* musicAnalysis);
 {
-
+    ofBackground(lavender);
 }
 
 void Background::changeBackground()
 {
-
+    //get the color of the terrain
+    ofColor terrain_color = terrain.getColor();
+    //create a new variable with the complement of the color
+    ofColor complement_terrain= terrain_color.invert();
+    //assign this complement to the current color variable of background
+    color=complement_terrain;
+    //draw the background with its color variable
+    ofBackground(color);
+    
 }
