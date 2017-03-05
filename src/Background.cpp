5eeaa80d050e3674d9346b7c8c7b9baafd1e91a8
Bryce Matsumori <bmats@users.noreply.github.com>
2017-03-04 17:52:29 -0800

    Fix merge things

--- src/Background.cpp
+++ src/Background.cpp
@@ -1,19 +1,15 @@
 #include "Background.h"
 
-<<<<<<< HEAD
-Background::Background(MusicAnalysis* musicAnalysis);
-=======
 Background::Background(MusicAnalysis* analysis)
 	: musicAnalysis(analysis)
->>>>>>> origin/master
 {
-    ofBackground(lavender);
+    ofBackground(ofColor::lavender);
 }
 
 void Background::changeBackground()
 {
     //get the color of the terrain
-    ofColor terrain_color = terrain.getColor();
+    ofColor terrain_color = terrain->getColor();
     //create a new variable with the complement of the color
     ofColor complement_terrain= terrain_color.invert();
     //assign this complement to the current color variable of background
